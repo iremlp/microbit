b5da5aed2deb2065aa24d9f17e0ba860373dd18f
Pskalou <pascal.padilla@gmail.com>
2018-05-09 23:42:02 +0200

    coloration friendly

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo','IPython.sphinxext.ipython_console_highlighting']
+extensions = ['sphinx.ext.todo']
 
 
 # Add any paths that contain templates here, relative to this directory.
@@ -90,7 +90,7 @@ exclude_patterns = []
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'friendly'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
